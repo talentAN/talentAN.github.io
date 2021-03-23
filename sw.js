e9cf91a6e87dc6bb631c1bb5c611821ad99b3e96
anzhen <anzhen@sensorsdata.com>
2021-03-23 10:14:32 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1ff801d405540b8d09f3.js"
+    "url": "webpack-runtime-9badebba8979364a29e6.js"
   },
   {
     "url": "framework-b17a64ad725e0b3c1338.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9e05fda8145f23d34e7c2e5ed27ae4ad"
+    "revision": "550bd5527d50d6d8bef5004a1bcc3fee"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a95b272f8c25b9a8a758.js"
