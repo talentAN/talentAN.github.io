5695dd71026dad9978641d3798a787e063206afb
anzhen <anzhen@sensorsdata.com>
2021-07-18 22:24:48 +0800

    第二章修订

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -12,13 +12,7 @@ const Blog = ({ data }) => {
     <Layout className="outerPadding">
       <Layout className="container">
         <Header />
-        <SEO
-          title="Blog"
-          description="I like blogging about various web technologies and other stuff related to
-            javascript and other trends like graphql, prisma etc. This blog expresses my views of various technologies
-            and scenarios I have come across in realtime."
-          path="blog"
-        />
+        <SEO title="Blog" description="good articles" path="blog" />
         <SidebarWrapper>
           <div className="marginTopTitle">
             <h1 className="titleSeparate">博文</h1>
