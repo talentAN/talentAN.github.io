ff2b3f4826b52b1001469573710f4d30fd1486e9
talentAN <talentAN@users.noreply.github.com>
2022-08-06 21:18:58 +0000

    deploy: df143889dd29ea6868412896b23771f6564185ba

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4c716a5362e42f0737d0.js"
+    "url": "webpack-runtime-afdadec5e968e3ae5430.js"
   },
   {
     "url": "framework-282cac162efadf614348.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3f6147d8eed35ad526ecbf9d4e65347d"
+    "revision": "c6fc2204c379b4f87fa033ddd40bb445"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-162eda8abf0539b5b76e.js"
