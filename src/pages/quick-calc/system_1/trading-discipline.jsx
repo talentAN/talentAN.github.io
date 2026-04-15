2dccb187842f6bcbbc33660abe3925e7e22dbc5d
zhen.an <az15921869329@gmail.com>
2026-04-15 15:05:28 +0800

    fix: 添加交易纪律页面路由文件

---
+++ src/pages/quick-calc/system_1/trading-discipline.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../index';
+import System1 from '../tabs/system_1/System1';
+
+const TradingDisciplinePage = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <System1 location={location} />
+    </Layout>
+  );
+};
+
+export default TradingDisciplinePage;
